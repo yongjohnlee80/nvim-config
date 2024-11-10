489eade811889c5305efb59bef3675521cd5b6ce
Yong Sung John Lee <yongjohnlee80@outlook.com>
2024-11-11 01:08:18 +0900

    Rust setup implemented in the neovim (#2)
    * rustacean added
    
    * rust setup
    
    * adding gitignore
    
    * removing unnecessary files
    
    * removing build files

---
+++ lua/johno/plugins/languages/rust.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"mrcjkb/rustaceanvim",
+		ft = "rust",
+		version = "^5", -- Recommended
+		lazy = false, -- This plugin is already lazy
+		config = function()
+			local mason_registry = require("mason-registry")
+			local codelldb = mason_registry.get_package("codelldb")
+			local extension_path = codelldb:get_install_path() .. "/extension/"
+			local codelldb_path = extension_path .. "adapter/codelldb"
+			local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+			local cfg = require("rustaceanvim.config")
+
+			vim.g.rustaceanvim = {
+				dap = {
+					adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+				},
+			}
+		end,
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+}
--- lua/johno/plugins/lsp/mason.lua
+++ lua/johno/plugins/lsp/mason.lua
@@ -50,6 +50,7 @@ return {
 				"gopls",
 				"delve",
 				"codelldb",
+				"rust-analyzer",
 			},
 		})
 	end,
