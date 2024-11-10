6418735caadf8be37809d9eba9594c6734d41524
johno <yongjohnlee80@outlook.com>
2024-11-10 08:03:58 -0800

    removing unnecessary files

---
+++ lua/samples/rust/hello_world/target/debug/deps/hello_world-0b9eb72901a07e40.d
@@ -0,0 +1,5 @@
+/home/johno/.config/nvim/lua/samples/rust/hello_world/target/debug/deps/libhello_world-0b9eb72901a07e40.rmeta: src/main.rs
+
+/home/johno/.config/nvim/lua/samples/rust/hello_world/target/debug/deps/hello_world-0b9eb72901a07e40.d: src/main.rs
+
+src/main.rs:
